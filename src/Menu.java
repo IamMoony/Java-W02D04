b675616dad083af95d45b8abc41170f1909bfa61
Simon Blaha <s.blaha@live.at>
2019-05-16 10:10:22 +0200

    push exercise 2

---
+++ src/Menu.java
@@ -0,0 +1,19 @@
+public class Menu {
+
+    String name;
+    int portionVolume;
+    int price;
+    String mainDish;
+    String desert;
+    String soup;
+
+
+    public Menu(String name, int price, int portionVolume) {
+        this.name = name;
+        this.portionVolume = portionVolume;
+        this.price = price;
+        this.mainDish = mainDish;
+        this.desert = desert;
+        this.soup = soup;
+    }
+}
---
+++ src/WeekdayMenu.java
@@ -0,0 +1,7 @@
+public class WeekdayMenu {
+
+    public WeekdayMenu(String name, int portionVolume, int price, String mainDish, String desert, String soup) {
+        super(name, portionVolume, price, mainDish, desert, soup);
+    }
+
+}
