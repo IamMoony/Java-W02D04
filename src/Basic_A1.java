8359ddc0a4d98233a73e41001231f9d6daba90c5
Simon Blaha <s.blaha@live.at>
2019-05-16 09:39:34 +0200

    finish basic exercise a1

--- src/Basic_A1.java
+++ src/Basic_A1.java
@@ -1,7 +1,16 @@
+import java.net.SocketTimeoutException;
+import java.sql.SQLOutput;
+
 public class Basic_A1 {
 
     public static void main(String[] args) {
 
-      
+        try {
+
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[10]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        }
     }
 }
